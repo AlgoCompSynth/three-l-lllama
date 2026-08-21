dba8761e566b4562906d061ccdb1e090526d9f23
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-08-21 00:24:38 -0700

    moar coding agents

--- installers/coding-agents.sh
+++ installers/coding-agents.sh
@@ -2,11 +2,18 @@
 
 set -eu
 
+mkdir --parents $HOME/Logfiles
+export LOGFILE=$HOME/Logfiles/coding-agents.log
+rm --force $LOGFILE
+
 echo "....Activating Homebrew PATH"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
 
 echo "....Installing brew packages"
+brew trust frostyard/tap
 brew install --yes --quiet \
+  block-goose-cli \
+  opencode \
   pi-coding-agent \
   >> $LOGFILE 2>&1
 
